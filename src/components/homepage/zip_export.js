043bd2ab5f8865c44344a686cf7848643463a7df
Drew Hemsley <drew@grow.com>
2020-05-11 14:10:29 -0600

    erasing and starting with floppy exports

--- src/components/homepage/zip_export.js
+++ src/components/homepage/zip_export.js
@@ -1,39 +1,12 @@
 import React from "react";
 import "../../stylings/container_styles/container_style.css";
-
+import "../../source_floppies/kq6/whole_game.zip";
 const ZipExport = (props) => {
-  const { name, order, height, weight, id, base_experience, sprites } = props.pokemon;
-  if (props.pokemon.length === 0 && props.buttonClicked === true) {
-    return (
-      <div className="wrapper-box">
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-  if (props.pokemon.length === 0) {
-    return (
-      <div styles={{ display: "flex", alignSelf: "center" }}>
-        Your Pokemon's Information will be Displayed Below
-      </div>
-    );
-  }
   return (
-    <div className="wrapper-box">
-      <div className="top-half-box">
-        {typeof name != "string" ? (
-          <div>Incompatible Name</div>
-        ) : (
-          <h1>{name.charAt(0).toUpperCase() + name.slice(1)}</h1>
-        )}
-      </div>
-      <div className="bottom-half-box">
-        <h2>Order #{order}</h2>
-        <h2>Height: {height} Decimetres </h2>
-        <h2>Weight: {weight} Hectograms </h2>
-        <h2>ID: {id} </h2>
-        <h2>Starting EXP: {base_experience}</h2>
-        <img src={sprites.front_shiny} alt="Pokemon Selected"></img>
-      </div>
+    <div>
+      <a href="whole_game.zip" download>
+        KQ6
+      </a>
     </div>
   );
 };
