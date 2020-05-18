a16b32f6e4c7b20678db67702cf65af7a9ac9f6d
Drew Hemsley <drew@grow.com>
2020-05-17 23:18:55 -0600

    added other games

--- src/components/homepage/zip_export.js
+++ src/components/homepage/zip_export.js
@@ -14,11 +14,32 @@ const ZipExport = (props) => {
   // };
   // you don't need the above with what you're doing below - the below is the 'easy' way
 
+  const container_style = {
+    display: "flex",
+    flexFlow: "row wrap",
+    width: "35%",
+    height: "50%",
+    justifyContent: "space-between",
+    fontSize: "20px",
+  };
+
+  // ALSO - declare your styles here as objects that you can make things way cleaner
+
   return (
     <div>
       <h1>Compressed Floppy Disk Downloads!</h1>
 
-      <a href="source_floppies/kq6/whole_game.zip">KQ6 Floppy Download</a>
+      <div style={container_style}>
+        <a href="source_floppies/kq6/whole_game.zip" download>
+          KQ6 Floppy Download
+        </a>
+        <a href="source_floppies/amon_ra/amon_ra.zip" download>
+          Amon Ra Floppy Download
+        </a>
+        <a href="source_floppies/kq2/kq2.zip">KQ2 Floppy Download</a>
+        <a href="source_floppies/kq3/kq3.zip">KQ3 Floppy Download</a>
+        <a href="source_floppies/kq4/kq4.zip">KQ4 Floppy Download</a>
+      </div>
     </div>
   );
 };
